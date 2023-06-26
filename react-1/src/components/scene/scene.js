99816341361185560a8f98397f40993c85cd73a7
Tina Decman <tina.decman@gmail.com>
2023-06-26 12:17:58 +0200

    exercise 1

---
+++ react-1/src/components/scene/scene.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Scene() {
+
+    return(
+        <div>
+            <p>El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial</p>
+        </div>
+    )
+}
+
+export default Scene; 
